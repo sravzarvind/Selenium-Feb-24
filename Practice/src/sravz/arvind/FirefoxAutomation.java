236918a23ed650b4b76d32d35649377964bbde0f
Sravzarvind <sravanthykrishna@gmail.com>
2018-03-07 06:49:33 +1100

    Created a practice project

---
+++ Practice/src/sravz/arvind/FirefoxAutomation.java
@@ -0,0 +1,5 @@
+package sravz.arvind;
+
+public class FirefoxAutomation {
+
+}
---
+++ Practice/src/sravz/arvind/SecondClass.java
@@ -0,0 +1,9 @@
+package sravz.arvind;
+
+
+public class SecondClass {
+	public static void main(String[] args) {
+		System.out.println("My first message");
+	}
+
+}
