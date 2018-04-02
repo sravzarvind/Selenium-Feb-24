cfe8fc6fa8371ee7f6d2ac006781b0827a1ec8ad
Sravzarvind <sravanthykrishna@gmail.com>
2018-04-03 07:48:38 +1000

    3rd April

--- Practice/src/sravz/arvind/FirefoxAutomation.java
+++ Practice/src/sravz/arvind/FirefoxAutomation.java
@@ -1,5 +1,11 @@
 package sravz.arvind;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FirefoxAutomation {
 
+	public static void main(String[] args) {
+		FirefoxDriver fd = new FirefoxDriver();
+		fd.get("https://www.gmail.com");
+	}
 }
---
+++ Practice/src/sravz/arvind/LoginTestcase.java
@@ -0,0 +1,36 @@
+package sravz.arvind;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTestcase {
+	void ValidUserNameandPwd()
+	{
+		WebDriver fd = new FirefoxDriver();
+		fd.get("www.gmail.com");
+		
+		
+	}
+	
+	void ValidUserNameandInvalidPwd()
+	{
+		
+	}
+	
+	void InvalidUserNameandInvalidPwd()
+	{
+		
+	}
+	
+	void InvalidUserNameandValidPwd()
+	{
+		
+	}
+	
+	void NoUserNameandPwd()
+	{
+		
+	}
+	
+
+}
